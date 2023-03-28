d8d66208c2abbab4db44a97bcfd91ee100075731
ilijaradojkovic <radojkovicika@gmail.com>
2023-03-28 23:43:59 +0200

    t

--- src/main/java/com/example/kafkastreams/service/KafkaProduerService.java
+++ src/main/java/com/example/kafkastreams/service/KafkaProduerService.java
@@ -3,8 +3,14 @@ package com.example.kafkastreams.service;
 import com.example.kafkastreams.model.Invoice;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 import static com.example.kafkastreams.config.Constants.TOPIC_NAME;
 
@@ -17,4 +23,10 @@ public class KafkaProduerService {
      log.info(String.format("Producing Message -Key: %s Value %s to topic %s",TOPIC_NAME,invoice.getMStoreID(),invoice));
         kafkaTemplate.send(TOPIC_NAME,invoice.getMStoreID(),invoice);
     }
+
+    @Bean
+    public Consumer<KStream<String,String>> process(){
+        return (x)->x.foreach((k,v)->System.out.format(k+" - "+ v));
+    }
+
 }
